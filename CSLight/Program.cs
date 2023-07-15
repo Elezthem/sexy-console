a4dfd2bf41b81607e8e04dd2a1e4bcb201fc3e6f
Elezthem <97851451+Elezthem@users.noreply.github.com>
2023-07-15 22:17:55 +0300

    Add files via upload

---
+++ CSLight/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(5, 10);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            Console.WindowHeight = 10;
+            Console.WindowWidth = 35;
+
+            Console.WriteLine("Привет Сергей!\nКак успехи?\nУспехи нормальные!...]\b \b");
+            //Console.Clear();
+
+            Console.ReadKey();
+        }
+    }
+}
